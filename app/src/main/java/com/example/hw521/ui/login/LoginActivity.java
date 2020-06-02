7cdbd8e17e7232e43e7a021af682740f4001fe1d
meowka <Pustygin.a@mail.ru>
2020-06-02 22:30:42 +0700

    refactor

--- app/src/main/java/com/example/hw521/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/hw521/ui/login/LoginActivity.java
@@ -7,19 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.hw521.R;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -32,80 +27,82 @@ public class LoginActivity extends AppCompatActivity {
     String loginFile;
     String passwordFile;
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         init();
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FileOutputStream fileOutputStream = null;
-                FileOutputStream fileOutputStreamPass = null;
-                try {
-                    fileOutputStream = openFileOutput(loginFile, MODE_PRIVATE);
-                    fileOutputStreamPass = openFileOutput(passwordFile, MODE_PRIVATE);
-                    OutputStreamWriter outputStreamWriterPass = new OutputStreamWriter(fileOutputStreamPass);
-                    BufferedWriter bufferedWriterPass = new BufferedWriter(outputStreamWriterPass);
-                    bufferedWriterPass.write(passwordStr);
-
-                    
-                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-                    BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-                    bufferedWriter.write(usernameStr);
-                    bufferedWriter.close();
-                    bufferedWriterPass.close();
-                    Toast.makeText(LoginActivity.this,"register done!",Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FileInputStream fileInputStream = null;
-                FileInputStream fileInputStreamPass = null;
-                try {
-                    fileInputStream = openFileInput(loginFile);
-                    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-                    BufferedReader readerLogin = new BufferedReader(inputStreamReader);
-                    fileInputStreamPass = openFileInput(passwordFile);
-                    InputStreamReader inputStreamReaderPass = new InputStreamReader(fileInputStreamPass);
-                    BufferedReader readerPass = new BufferedReader(inputStreamReaderPass);
-                    String openLogin = readerLogin.readLine();
-                    String openPassword = readerPass.readLine();
-                    if (openLogin.equals(usernameStr) && openPassword.equals(passwordStr)){
-                        Toast.makeText(LoginActivity.this,"welcome",Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        Toast.makeText(LoginActivity.this,"Error login or password",Toast.LENGTH_SHORT).show();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
     }
 
-    private void init(){
+    private void init() {
         usernameEditText = findViewById(R.id.username);
         passwordEditText = findViewById(R.id.password);
         loginButton = findViewById(R.id.login);
         registerButton = findViewById(R.id.register);
         loginFile = "login_file.txt";
         passwordFile = "password_file.txt";
-        usernameStr = usernameEditText.getText().toString();
-        passwordStr = passwordEditText.getText().toString();
-
     }
 
+    public void registerClick(View view) {
+        FileOutputStream fileOutputStream = null;
+        FileOutputStream fileOutputStreamPass = null;
+        try {
+            usernameStr = usernameEditText.getText().toString();
+            passwordStr = passwordEditText.getText().toString();
+            if (usernameStr.equals("") || passwordStr.equals("")) {
+                Toast.makeText(LoginActivity.this, "Error login or password", Toast.LENGTH_SHORT).show();
+            } else {
+                fileOutputStream = openFileOutput(loginFile, MODE_PRIVATE);
+                fileOutputStreamPass = openFileOutput(passwordFile, MODE_PRIVATE);
+                fileOutputStream.write(usernameStr.getBytes());
+                fileOutputStreamPass.write(passwordStr.getBytes());
+                Toast.makeText(LoginActivity.this, "register done!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null && fileOutputStreamPass != null) {
+                try {
+                    fileOutputStream.close();
+                    fileOutputStreamPass.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
+    public void loginClick(View view) {
 
+        FileInputStream fileInputStream = null;
+        FileInputStream fileInputStreamPass = null;
+        try {
+            usernameStr = usernameEditText.getText().toString();
+            passwordStr = passwordEditText.getText().toString();
+            fileInputStream = openFileInput(loginFile);
+            byte[] bytesLogin = new byte[fileInputStream.available()];
+            fileInputStream.read(bytesLogin);
+            fileInputStreamPass = openFileInput(passwordFile);
+            byte[] bytePass = new byte[fileInputStreamPass.available()];
+            fileInputStreamPass.read(bytePass);
+            String openLogin = new String(bytesLogin);
+            String openPassword = new String(bytePass);
+            if (openLogin.equals(usernameStr) && openPassword.equals(passwordStr)) {
+                Toast.makeText(LoginActivity.this, "welcome", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LoginActivity.this, "Error login or password", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null && fileInputStreamPass != null) {
+                try {
+                    fileInputStream.close();
+                    fileInputStreamPass.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
